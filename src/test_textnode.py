f3ec65172bf827ceaba5560f850da9a42295f618
DadBytes <bpmckim@proton.me>
2025-05-05 14:55:20 -0400

    add tests for TextNode class

---
+++ src/test_textnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_ep(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.NORMAL)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        expected_output = "TextNode('This is a text node', bold, None)"
+        self.assertEqual(repr(node), expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class TextType(Enum):
     NORMAL = "normal"
+    BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
