f8e6f37ca7bf679195029ac206c1fdb91e5a36c2
DadBytes <bpmckim@proton.me>
2025-05-05 14:37:23 -0400

    add TextNode & TextType classes

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    # Create a TextNode object with some text and type
+    text_node = TextNode("Hello, World!", TextType.ITALIC)
+    print(text_node)
+
+
+main()
